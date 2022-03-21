b8471e82670f0ebad3fefbfc437d0b49117ca1ce
Thomas <tsvanmeer@outlook.com>
2022-03-21 15:05:08 +0100

    Code 1.1

--- robotcode.cpp
+++ robotcode.cpp
@@ -1,5 +1,5 @@
 //Arduino PWM Speed Control：
-int E1 = 5;
+int E1 = 5; //
 int M1 = 4;
 int E2 = 6;
 int M2 = 7;
@@ -8,13 +8,19 @@ void setup()
 {
     pinMode(M1, OUTPUT);
     pinMode(M2, OUTPUT);
+    
+    pinMode(A0, INPUT);
+    pinMode(A1, INPUT);
+    pinMode(A2, INPUT);
+    pinMode(A3, INPUT);
+    pinMode(A4, INPUT);
 
-
+    Serial.begin(9600);
 }
 
 void loop()
 {
-
+    // WIEL BESTURING
     digitalWrite(M1,LOW);
     digitalWrite(M2, HIGH);
     analogWrite(E1,70);   //PWM Speed Control
@@ -38,5 +44,14 @@ void loop()
     analogWrite(E2,10);   //PWM Speed Control
     delay(500);
 
+    // LIJN SENSOR
+    analogRead(A0);
+    Serial.println(analogRead(A0));
+    Serial.println(analogRead(A1));
+    Serial.println(analogRead(A2));
+    Serial.println(analogRead(A3));
+    Serial.println(analogRead(A4));
+
+    // PING SENSOR
 
 }
