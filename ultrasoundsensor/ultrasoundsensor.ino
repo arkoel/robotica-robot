4f733aaf45ecfec15980a18159d6f0521758649f
s.groenendal01@gmail.com <s.groenendal01@gmail.com>
2022-03-24 16:21:25 +0100

    changes

--- ultrasoundsensor/ultrasoundsensor.ino
+++ ultrasoundsensor/ultrasoundsensor.ino
@@ -1,5 +1,5 @@
 #define echoPin A5 // attach pin D2 Arduino to pin Echo of HC-SR04
-#define trigPin 0 // attach pin D3 Arduino to pin Trig of HC-SR04
+#define trigPin 0  // attach pin D3 Arduino to pin Trig of HC-SR04
 
 // defines variables
 long duration; // variable for the duration of sound wave travel
@@ -9,7 +9,18 @@ int E1 = 5;
 int M1 = 4;
 int E2 = 6;
 int M2 = 7;
-
+byte segValue[10][7] = {
+    {1, 1, 1, 1, 1, 1, 0}, // 0
+    {1, 0, 0, 1, 0, 0, 0}, // 1
+    {0, 1, 1, 1, 1, 0, 1}, // 2
+    {1, 1, 0, 1, 1, 0, 1}, // 3
+    {1, 0, 0, 1, 0, 1, 1}, // 4
+    {1, 1, 0, 0, 1, 1, 1}, // 5
+    {1, 1, 1, 0, 1, 1, 1}, // 6
+    {1, 0, 0, 1, 1, 0, 0}, // 7
+    {1, 1, 1, 1, 1, 1, 1}, // 8
+    {1, 1, 0, 1, 1, 1, 1}  // 9
+};
 boolean lijnsensoren[5];
 
 boolean startup = true;
@@ -17,7 +28,7 @@ static const uint8_t analog_pins[] = {A0, A1, A2, A3, A4};
 boolean state = 1;
 // state 1=straight, 0=turning
 int difference;
-
+int turns;
 void setup()
 {
   pinMode(trigPin, OUTPUT); // Sets the trigPin as an OUTPUT
@@ -37,128 +48,174 @@ void setup()
 
 void loop()
 {
-
   if (state)
   {
     for (int i = 0; i < 5; i++)
     {
-      if(analogRead(analog_pins[i]) < 200){
+      if (analogRead(analog_pins[i]) < 200)
+      {
         lijnsensoren[i] = 1;
       }
-      else if(analogRead(analog_pins[i]) > 200){
+      else if (analogRead(analog_pins[i]) > 200)
+      {
         lijnsensoren[i] = 0;
       }
-        // Serial.println(analogRead(analog_pins[i]));
-        // delay(1000);
+      // Serial.println(analogRead(analog_pins[i]));
+      // delay(1000);
     }
 
     // Serial.println(sum(lijnsensoren, 5));
     // delay(1000);
 
-    int sumlijnsesoren = sum(lijnsensoren, 5);
+    int sumlijnsensoren = sum(lijnsensoren, 5);
 
-    if (sumlijnsesoren < 3 && sumlijnsesoren > 0)
+    if (sumlijnsensoren < 3 && sumlijnsensoren > 0)
     {
       difference = 2 - getNumber(lijnsensoren, 1);
       if (difference < 0)
       {
-        digitalWrite(M1, LOW);
-        digitalWrite(M2, LOW);
-        turn(abs(difference) * 100);
+        if (turns < 9){turns++;}
+        dispnum(turns);
+        dispr(false);
+        turnLeft(difference);
       }
       else if (difference > 0)
       {
-        digitalWrite(M1, HIGH);
-        digitalWrite(M2, HIGH);
-        turn(abs(difference) * 100);
+        if (turns < 9){turns++;}
+          dispnum(turns);
+          dispr(true);
+          turnRight(difference);
+        }
+        else
+        {
+          digitalWrite(M1, LOW);
+          digitalWrite(M2, HIGH);
+          speed(100);
+        }
+
+        // if (sumlijnsensoren == 5)
+        // {
+        //   turnRight();
+        // }
       }
-      else
-      {
-        digitalWrite(M1, LOW);
-        digitalWrite(M2, HIGH);
-      }
+      // else if (sumlijnsensoren == 0) {
+      //     analogWrite(E1, 0); // PWM Speed Control
+      //     analogWrite(E2, 0); // PWM Speed Control
+      //     one_eighty(400);
+      // }
+      // if (distance < 10)
+      // {
+      //   analogWrite(E1, 0);
+      //   analogWrite(E2, 0);
+      //   one_eighty(100);
+      // }
     }
   }
 
-  // // Start driving, slow down when object is near
-  //   digitalWrite(M1, LOW);
-  //   digitalWrite(M2, HIGH);
-
-  // if(distance < 10) {
-  //   analogWrite(E1, 0);
-  //   analogWrite(E2, 0);
-  //   delay(1000);
-  //   startup=true;
-  //   start();
-  //   turn(1000);
-  //   startup=false;
-  // } else {
-  //   if(startup){
-  //   start();
-  //   startup=false;
-  //   }
-  // }
-}
-void start()
-{
-  analogWrite(E1, 70); // PWM Speed Control
-  analogWrite(E2, 70); // PWM Speed Control
-  delay(200);
-  analogWrite(E1, 55); // PWM Speed Control
-  analogWrite(E2, 55); // PWM Speed Control
-}
-
-void turn(int time)
-{
-  //digitalWrite(M1, HIGH);
-  //digitalWrite(M2, HIGH);
-  analogWrite(E1, 70); // PWM Speed Control
-  analogWrite(E2, 70); // PWM Speed Control
-  delay(time);
-  analogWrite(E1, 55); // PWM Speed Control
-  analogWrite(E2, 55); // PWM Speed Control
-}
-
-int sum(bool arr[], int n)
-{
-  int sum = 0; // initialize sum
-
-  // Iterate through all elements
-  // and add them to sum
-  for (int i = 0; i < n; i++) {
-    sum += int(arr[i]);
-  }
-  return sum;
-}
-
-int getNumber(bool arr[], int n)
-{
-  for (int i = 0; i < 5; i++)
+  void turnRight(int difference)
   {
-    if (arr[i] == 1)
+    digitalWrite(M1, HIGH);
+    digitalWrite(M2, HIGH);
+    turn(abs(difference) * 100);
+  }
+
+  void turnLeft(int difference)
+  {
+    digitalWrite(M1, LOW);
+    digitalWrite(M2, LOW);
+    turn(abs(difference) * 100);
+  }
+
+  void turn(int time)
+  {
+
+    analogWrite(E1, 70); // PWM Speed Control
+    analogWrite(E2, 70); // PWM Speed Control
+    delay(time);
+    analogWrite(E1, 50); // PWM Speed Control
+    analogWrite(E2, 50); // PWM Speed Control
+  }
+
+  void speed(int time)
+  {
+    // digitalWrite(M1, HIGH);
+    // digitalWrite(M2, HIGH);
+    analogWrite(E1, 70); // PWM Speed Control
+    analogWrite(E2, 70); // PWM Speed Control
+    delay(time);
+    analogWrite(E1, 35); // PWM Speed Control
+    analogWrite(E2, 35); // PWM Speed Control
+  }
+
+  int sum(bool arr[], int n)
+  {
+    int sum = 0; // initialize sum
+
+    // Iterate through all elements
+    // and add them to sum
+    for (int i = 0; i < n; i++)
     {
-      return i;
+      sum += int(arr[i]);
+    }
+    return sum;
+  }
+
+  int getNumber(bool arr[], int n)
+  {
+    for (int i = 0; i < 5; i++)
+    {
+      if (arr[i] == 1)
+      {
+        return i;
+      }
+    }
+    return 0;
+  }
+
+  int getdistance()
+  {
+    // Clears the trigPin condition
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+    // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+    // Reads the echoPin, returns the sound wave travel time in microseconds
+    duration = pulseIn(echoPin, HIGH);
+    // Calculating the distance
+    distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
+    // Displays the distance on the Serial Monitor
+    // Serial.print("Distance: ");
+    // Serial.print(distance);
+    // Serial.println(" cm");
+    return distance;
+  }
+
+  void dispnum(int n)
+  {
+    int m = 0;
+    for (int i = 3; i < 14; i++)
+    {
+      digitalWrite(i, segValue[n][m]);
+      m++;
+      if (i == 3)
+      {
+        i = i + 4;
+      }
     }
   }
-  return 0;
-}
 
-int getdistance()
-{
-  // Clears the trigPin condition
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  // Reads the echoPin, returns the sound wave travel time in microseconds
-  duration = pulseIn(echoPin, HIGH);
-  // Calculating the distance
-  distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
-  // Displays the distance on the Serial Monitor
-  // Serial.print("Distance: ");
-  // Serial.print(distance);
-  // Serial.println(" cm");
-  return distance;
-}
+  void dispr(bool n)
+  {
+    if (n)
+    {
+      digitalWrite(1, HIGH);
+      digitalWrite(2, HIGH);
+    }
+    else
+    {
+      digitalWrite(1, LOW);
+      digitalWrite(2, HIGH);
+    }
+  }
