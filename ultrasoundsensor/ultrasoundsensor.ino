d2ff0b0d11c7e95cea60a9a3e6d5a96f1214a234
Arkoel <arjenvanwijk03@gmail.com>
2022-03-26 16:55:53 +0100

    zaterdag progess

--- ultrasoundsensor/ultrasoundsensor.ino
+++ ultrasoundsensor/ultrasoundsensor.ino
@@ -1,7 +1,6 @@
 #define echoPin A5 // attach pin D2 Arduino to pin Echo of HC-SR04
-#define trigPin 0 // attach pin D3 Arduino to pin Trig of HC-SR04
+#define trigPin 0  // attach pin D3 Arduino to pin Trig of HC-SR04
 
-// defines variables
 long duration; // variable for the duration of sound wave travel
 int distance;  // variable for the distance measurement
 
@@ -11,8 +10,8 @@ int E2 = 6;
 int M2 = 7;
 
 bool oplossingsRoute[100];
-
-
+int bochtNr = 0;
+int oplossingSom = 0;
 
 byte segValue[13][7] = {
     {1, 1, 1, 1, 1, 1, 0}, // 0
@@ -25,20 +24,20 @@ byte segValue[13][7] = {
     {1, 0, 0, 1, 1, 0, 0}, // 7
     {1, 1, 1, 1, 1, 1, 1}, // 8
     {1, 1, 0, 1, 1, 1, 1}, // 9
-    {1, 0, 1, 0, 1, 0, 0}, // obstakel ({[([{({[({[({[({[({[{[[[[[(([{[{{({({({({([[[[[[([([{{3}}])])]]]]]])})})})})}}]}]))]]]]]}]})]})]})]})]})}])]})
+    {1, 0, 1, 0, 1, 0, 0}, // obstakel
     {1, 0, 1, 0, 0, 0, 1}, // DOODLOPEND PAD
     {0, 0, 1, 0, 1, 1, 1}  // F
 }; 
-boolean lijnsensoren[5];
 
+boolean lijnsensoren[5];
 boolean startup = true;
 static const uint8_t analog_pins[] = {A0, A1, A2, A3, A4};
 boolean state = 1;
 // state 1=straight, 0=turning
 int difference;
+int turns = 0;
 
-void setup()
-{
+void setup() {
   pinMode(trigPin, OUTPUT); // Sets the trigPin as an OUTPUT
   pinMode(echoPin, INPUT);  // Sets the echoPin as an INPUT
   // Serial.begin(9600); // // Serial Communication is starting with 9600 of baudrate speed
@@ -52,107 +51,153 @@ void setup()
   pinMode(A2, INPUT);
   pinMode(A3, INPUT);
   pinMode(A4, INPUT);
-  for(int i = 1; i<14;i++){
-    if(i<4||i>7){
-      pinMode(i,OUTPUT);
+  for(int l = 1; l<14; l++) {
+    if(l<4 || l>7) {
+      pinMode(l, OUTPUT);
     }
   }
 }
 
-void loop()
-{
-dispr(false);
-  if(state){
-    dispnum(12); //display stars
-    for (int i = 0; i < 5; i++)
-    {
-      if(analogRead(analog_pins[i]) < 200){
+void loop() {
+  dispnum(turns);
+  if (state) {
+    for (int i = 0; i < 5; i++) {
+      if (analogRead(analog_pins[i]) < 200) {
         lijnsensoren[i] = 1;
       }
-      else if(analogRead(analog_pins[i]) > 200){
+      else if (analogRead(analog_pins[i]) > 200) {
         lijnsensoren[i] = 0;
       }
-        // Serial.println(analogRead(analog_pins[i]));
-        // delay(1000);
+      // Serial.println(analogRead(analog_pins[i]));
+      // delay(1000);
     }
-
     distance = getdistance();
     // PING SENSOR
-    if (distance < 6) {
+    if (distance < 4 && turns>0) {
+      dispnum(10);
+      digitalWrite(1, LOW);
+      digitalWrite(2, LOW);
       analogWrite(E1, 0);
       analogWrite(E2, 0);
-
-      delay(200);
-      digitalWrite(M1, HIGH);
-      digitalWrite(M2, HIGH);
-      turn(750);
+      delay(750);
+      turnRight(4);
+      // delay(250);
     }
 
     // Serial.println(sum(lijnsensoren, 5));
     // delay(1000);
 
     int sumlijnsensoren = sum(lijnsensoren, 5);
-
-    if (sumlijnsensoren < 3 && sumlijnsensoren > 0)
-    {
-      difference = 2 - getNumber(lijnsensoren, 1);
-      if (difference < 0)
-      {
-        digitalWrite(M1, LOW);
-        digitalWrite(M2, LOW);
-        turn(abs(difference) * 100);
-      }
-      else if (difference > 0)
-      {
+    if (sumlijnsensoren < 3 && sumlijnsensoren > 0) {
+      difference = 2 - getNumber(lijnsensoren);
+      if (difference < 0) {
+        if(abs(difference)==2){
+          if (turns < 9){turns++;}
+          dispr(false);
+          bochtNr++;
+          oplossingSom++;
+          oplossingsRoute[bochtNr] = 0;
+        }
+        turnLeft(difference);
+      } else if (difference > 0) {
+        if(abs(difference)==2){
+          if (turns < 9){turns++;}
+          dispr(true);
+          bochtNr++;
+          oplossingSom++;
+          oplossingsRoute[bochtNr] = 1;
+        }
+        turnRight(difference);
+      }else{
         digitalWrite(M1, HIGH);
-        digitalWrite(M2, HIGH);
-        turn(abs(difference) * 100);
+        digitalWrite(M2, LOW);
+        speed(250);
       }
-      else
-      {
-        digitalWrite(M1, LOW);
-        digitalWrite(M2, HIGH);
-      }
-  
-    }else if(sumlijnsensoren==0){
-      dispnum(11); //display void
-
     }
-        if(sumlijnsensoren == 5){
-        stop();
-        state = false;
-        
+    if (sumlijnsensoren == 5) { // FINSIH
+      digitalWrite(M1, HIGH);
+      digitalWrite(M2, LOW);
+      speed(300);
+      analogWrite(E1, 0);
+      analogWrite(E2, 0);
+      for (int i = 0; i < 5; i++) {
+        if (analogRead(analog_pins[i]) < 200) {
+          lijnsensoren[i] = 1;
+        } else if (analogRead(analog_pins[i]) > 200) {
+          lijnsensoren[i] = 0;
+        }
       }
-  }
+      int sumlijnsensoren = sum(lijnsensoren, 5);
+      if (sumlijnsensoren == 5) { // FINSIH
+
+     
+      state = false;
+      
+      //Fi display
+      dispnum(12);
+      dispr(0);
+      delay(1000);
+      
+      // Laat route zien (bijv. L, L, R, L, R)
+      for(int i=0; i<oplossingSom; i++) {
+        digitalWrite(1, LOW);
+        digitalWrite(2, LOW);
+        delay(400);
+        dispr(oplossingsRoute[i]);
+        delay(1000);
+      }
+      dispnum(12);
+      dispr(0);
+      delay(9000);
+      } else{
+      //omdraaien toch geen finish
+
+      }
+    }
+    if(sumlijnsensoren == 0) {
+      digitalWrite(M1, LOW);
+      digitalWrite(M2, HIGH);
+      dispnum(11);
+      // exit(0);
+      //delay(1000);
+      turnRight(4);
+    }
+  } 
 }
 
-void start()
-{
-  analogWrite(E1, 70); // PWM Speed Control
-  analogWrite(E2, 70); // PWM Speed Control
-  delay(200);
-  analogWrite(E1, 55); // PWM Speed Control
-  analogWrite(E2, 55); // PWM Speed Control
+void turnRight(int difference) {
+  digitalWrite(M1, HIGH);
+  digitalWrite(M2, HIGH);
+  turn(abs(difference) * 150);
 }
 
-void stop(){
-  analogWrite(E1, 0); // PWM Speed Control
-  analogWrite(E2, 0); // PWM Speed Control
+void turnLeft(int difference) {
+  digitalWrite(M1, LOW);
+  digitalWrite(M2, LOW);
+  turn(abs(difference) * 150);
 }
 
-void turn(int time)
-{
-  //digitalWrite(M1, HIGH);
-  //digitalWrite(M2, HIGH);
-  analogWrite(E1, 70); // PWM Speed Control
-  analogWrite(E2, 70); // PWM Speed Control
-  delay(time);
-  analogWrite(E1, 50); // PWM Speed Control
-  analogWrite(E2, 50); // PWM Speed Control
+void turn(int time) {
+  analogWrite(E1, 70);
+  analogWrite(E2, 75); 
+  delay(100);
+  analogWrite(E1, 40); 
+  analogWrite(E2, 40); 
+  delay(time-100);
+  analogWrite(E1, 0);
+  analogWrite(E2, 0);
 }
 
-int sum(bool arr[], int n)
-{
+void speed(int time) {
+  analogWrite(E1, 70); 
+  analogWrite(E2, 70);
+  delay(100);
+  analogWrite(E1, 35);
+  analogWrite(E2, 35);
+  delay(time-100);
+}
+
+int sum(bool arr[], int n) {
   int sum = 0; // initialize sum
 
   // Iterate through all elements
@@ -163,20 +208,20 @@ int sum(bool arr[], int n)
   return sum;
 }
 
-int getNumber(bool arr[], int n)
-{
-  for (int i = 0; i < 5; i++)
-  {
-    if (arr[i] == 1)
-    {
-      return i;
+int getNumber(bool arr[]) {
+  int max = 0;
+  for (int i = 0; i < 5; i++) {
+    if (arr[i] == 1) {
+      if(max>abs(2-i)||i!=0){
+        max = i;
+      }
+      //return i;
     }
   }
-  return 0;
+  return max;
 }
 
-int getdistance()
-{
+int getdistance() {
   // Clears the trigPin condition
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
@@ -194,30 +239,25 @@ int getdistance()
   // Serial.println(" cm");
   return distance;
 }
-  void dispnum(int n)
-  {
-    int m = 0;
-    for (int i = 3; i < 14; i++)
-    {
-      digitalWrite(i, segValue[n][m]);
-      m++;
-      if (i == 3)
-      {
-        i = i + 4;
-      }
-    }
-  }
 
-  void dispr(bool n)
-  {
-    if (n)
-    {
-      digitalWrite(1, HIGH);
-      digitalWrite(2, HIGH);
-    }
-    else
-    {
-      digitalWrite(1, LOW);
-      digitalWrite(2, HIGH);
+void dispnum(int n) {
+  int m = 0;
+  for (int i = 3; i < 14; i++) {
+    digitalWrite(i, segValue[n][m]);
+    m++;
+    if (i == 3) {
+      i = i + 4;
     }
   }
+}
+
+void dispr(bool n) {
+  if (n) {
+    digitalWrite(1, HIGH);
+    digitalWrite(2, HIGH);
+  }
+  else {
+    digitalWrite(1, LOW);
+    digitalWrite(2, HIGH);
+  }
+}
